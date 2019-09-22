e31a903d51b3d7ec3058c1da87a7a093b33fb494
pcysl5edgo <pcysl5edgo@gmail.com>
2019-09-23 04:16:33 +0900

    [update]Test

---
+++ Assets/Tests/NewTestScript.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        [Test]
+        public void IsTrueTest()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+
+        [Test]
+        public void IsFalseTest()
+        {
+            Assert.IsFalse(1 == 100);
+        }
+    }
+}
