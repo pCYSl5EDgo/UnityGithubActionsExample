2d7bcdd11b9b8b6e3a201314af12a4c081290745
pcysl5edgo <pcysl5edgo@gmail.com>
2019-09-23 18:03:34 +0900

    Fail Proof

--- Assets/Tests/NewTestScript.cs
+++ Assets/Tests/NewTestScript.cs
@@ -4,16 +4,10 @@ namespace Tests
 {
     public class NewTestScript
     {
-        [Test]
-        public void IsTrueTest()
-        {
-            Assert.IsTrue(1 == 1);
-        }
+        [Test] public void IsTrueTest() => Assert.IsTrue(true);
 
-        [Test]
-        public void IsFalseTest()
-        {
-            Assert.IsFalse(1 == 100);
-        }
+        [Test] public void IsFalseTest() => Assert.IsFalse(false);
+
+        [Test] public void FailProof() => Assert.IsTrue(false);
     }
 }
