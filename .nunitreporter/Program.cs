91f49f064c8dc18a1c349076f21497918fe22ed3
pcysl5edgo <pcysl5edgo@gmail.com>
2019-09-24 20:29:11 +0900

    File

--- .nunitreporter/Program.cs
+++ .nunitreporter/Program.cs
@@ -8,12 +8,16 @@ namespace NUnitReporter
 {
     class Program
     {
-        static int Main(string[] args)
+        static void Main(string[] args)
         {
             var doc = new XmlDocument();
             doc.LoadXml(File.ReadAllText(args[0], Encoding.UTF8));
-            Console.Write(Encoding.UTF8.GetString(JsonSerializer.Serialize(new JsonEncoder().Encode(doc, out var success))));
-            return success ? 0 : 1;
+            if (args[1] == "--json")
+            {
+                Console.Write(@"{""text"":");
+                Console.Write(Encoding.UTF8.GetString(JsonSerializer.Serialize(new JsonEncoder().Encode(doc, out _))));
+                Console.Write('}');
+            }
         }
     }
 }
