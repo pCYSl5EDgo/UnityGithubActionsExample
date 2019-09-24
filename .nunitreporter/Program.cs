3e4eae44bf05077812aa12d9fbe0b61db61d6c60
pcysl5edgo <pcysl5edgo@gmail.com>
2019-09-24 20:49:26 +0900

    Pure

--- .nunitreporter/Program.cs
+++ .nunitreporter/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
 using System.IO;
 using System.Xml;
 using Utf8Json;
@@ -8,15 +7,22 @@ namespace NUnitReporter
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             var doc = new XmlDocument();
             doc.LoadXml(File.ReadAllText(args[0], Encoding.UTF8));
-            if (args[1] == "--json")
+            switch (args[1])
             {
-                Console.Write(@"{""text"":");
-                Console.Write(Encoding.UTF8.GetString(JsonSerializer.Serialize(new JsonEncoder().Encode(doc, out _))));
-                Console.Write('}');
+                case "--json":
+                    {
+                        using var writer = new StreamWriter(args[2]);
+                        writer.Write(@"{""text"":");
+                        writer.Write(Encoding.UTF8.GetString(JsonSerializer.Serialize(new JsonEncoder().Encode(doc, out _))));
+                        writer.Write('}');
+                        return 0;
+                    }
+                default:
+                    return 1;
             }
         }
     }
