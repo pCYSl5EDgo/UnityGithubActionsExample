df4405f4f50d4900919fb3e0b5b6a48a3fbc5e58
pcysl5edgo <pcysl5edgo@gmail.com>
2019-09-25 09:32:21 +0900

    Hook Console

--- .nunitreporter/Program.cs
+++ .nunitreporter/Program.cs
@@ -24,7 +24,10 @@ namespace NUnitReporter
                     }
                 case "--console":
                     {
-                        Console.WriteLine(new ConsoleEncoder().Encode(doc, out _));
+                        using var writer = new StreamWriter(args[2]);
+                        writer.Write(@"{""text"":");
+                        writer.Write(Encoding.UTF8.GetString(JsonSerializer.Serialize(new ConsoleEncoder().Encode(doc, out _))));
+                        writer.Write('}');
                         return 0;
                     }
                 default:
