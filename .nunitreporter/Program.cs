9267cc6a77edbac2d2329cf1289d7d0d11fbaf87
pcysl5edgo <pcysl5edgo@gmail.com>
2019-09-25 11:08:45 +0900

    FAIL

--- .nunitreporter/Program.cs
+++ .nunitreporter/Program.cs
@@ -18,17 +18,17 @@ namespace NUnitReporter
                     {
                         using var writer = new StreamWriter(args[2]);
                         writer.Write(@"{""text"":");
-                        writer.Write(Encoding.UTF8.GetString(JsonSerializer.Serialize(new JsonEncoder().Encode(doc, out _))));
+                        writer.Write(Encoding.UTF8.GetString(JsonSerializer.Serialize(new JsonEncoder().Encode(doc, out var success))));
                         writer.Write('}');
-                        return 0;
+                        return success ? 0 : 2;
                     }
                 case "--console":
                     {
                         using var writer = new StreamWriter(args[2]);
                         writer.Write(@"{""text"":");
-                        writer.Write(Encoding.UTF8.GetString(JsonSerializer.Serialize(new ConsoleEncoder().Encode(doc, out _))));
+                        writer.Write(Encoding.UTF8.GetString(JsonSerializer.Serialize(new ConsoleEncoder().Encode(doc, out var success))));
                         writer.Write('}');
-                        return 0;
+                        return success ? 0 : 2;
                     }
                 default:
                     return 1;
